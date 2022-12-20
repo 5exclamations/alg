0d3c4d2f6e9187d629670235e6ef4154fad6206d
farman1703 <farman.guseynov.291@gmail.com>
2022-12-21 03:38:01 +0400

    1

---
+++ SQL/server.js
@@ -0,0 +1,27 @@
+const mysql = require("mysql");
+const express = require("express");
+const app = express();
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "murad2002",
+  database: "task",
+});
+
+connection.connect((err) => {
+  if (!err) console.log("Sucsess");
+  else console.log(err.sqlMessage);
+});
+
+app.get("/", (req, res) => {
+  connection.query(`SELECT * FROM clients`, (err, data) => {
+    if (err) return res.status(500);
+    res.json(data);
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Example app listening on port 3000!");
+});
